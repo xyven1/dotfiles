fe58f173417d44cf7b7de2766207103cb9458410
Xyven <35360746+Xyven1@users.noreply.github.com>
2022-05-25 00:19:41 -0700

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt upgrade
+sudo apt install build-essential
+
+
+rm -rf ~/.asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
+echo $'\n# asdf config' >> ~/.bashrc
+echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+. ~/.bashrc
+
+cat .tool-versions | cut -d' ' -f1 | grep "^[^\#]" | xargs -i asdf plugin add  {}
