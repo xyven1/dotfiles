dab5a95c8e5cb5f5f85007b621f452a787d88b0c
Xyven <35360746+Xyven1@users.noreply.github.com>
2022-05-25 00:20:22 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 sudo apt update && sudo apt upgrade
 sudo apt install build-essential
 
+echo `neovim 0.7.0
+fzf 0.30.0
+direnv 2.31.0
+gitui 0.20.1` >> .tool-versions
 
 rm -rf ~/.asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
