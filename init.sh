cc768ea6af7d984bdaf2a9f4ce5e0e2dedf04f81
Xyven <35360746+Xyven1@users.noreply.github.com>
2022-10-18 14:19:36 -0400

    updated init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,18 @@
-sudo apt update && sudo apt upgrade
-sudo apt install build-essential
+sudo apt update && sudo apt upgrade -y
+sudo apt install build-essential -y
 
-echo `neovim 0.7.0
-fzf 0.30.0
-direnv 2.31.0
-gitui 0.20.1` >> .tool-versions
+echo `neovim nightly
+fzf latest
+direnv latest
+gitui latest` >> .tool-versions
 
 rm -rf ~/.asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+asdf update
 echo $'\n# asdf config' >> ~/.bashrc
 echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 . ~/.bashrc
 
 cat .tool-versions | cut -d' ' -f1 | grep "^[^\#]" | xargs -i asdf plugin add  {}
+asdf install
