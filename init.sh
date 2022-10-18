01706349a8811f4f8d4b744ecb570236d95b6dce
Xyven <35360746+Xyven1@users.noreply.github.com>
2022-10-18 14:22:45 -0400

    now expecting user to have cloned repo

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install build-essential -y
 
-echo `neovim nightly
-fzf latest
-direnv latest
-gitui latest` >> .tool-versions
+cat .tool-versions >> ~/.tool-versions
+cat .bashrc >> ~/.bashrc
+cat .bash_aliases >> ~/.bash_aliases
 
 rm -rf ~/.asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
@@ -14,5 +13,5 @@ echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 . ~/.bashrc
 
-cat .tool-versions | cut -d' ' -f1 | grep "^[^\#]" | xargs -i asdf plugin add  {}
+cat ~/.tool-versions | cut -d' ' -f1 | grep "^[^\#]" | xargs -i asdf plugin add {}
 asdf install
