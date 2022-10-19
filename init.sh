3ebd009ef1106f2f1f559f1521d84626eb759ca5
Xyven <35360746+Xyven1@users.noreply.github.com>
2022-10-19 02:28:30 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -23,5 +23,4 @@ cat ~/.tool-versions | cut -d' ' -f1 | grep "^[^\#]" | xargs -i asdf global {} l
 rm -rf ~/.temp_init
 
 asdf direnv setup --shell bash --version latest
-. ~/.bashrc
-. ~/.profile
+echo "Done setting things up. Please restart your machine to get everything working"
