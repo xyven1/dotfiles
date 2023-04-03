25633251f8d0a9df7e5cea65a2dc481321c61695
Xyven <git@xyven.dev>
2023-04-03 11:58:49 -0400

    Update init.sh
    just source asdf

--- init.sh
+++ init.sh
@@ -14,7 +14,7 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 echo '\n# asdf config' >> ~/.bashrc
 echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
-source ~/.bashrc
+. ~/.asdf/asdf.sh
 asdf update
 
 cat ~/.tool-versions | cut -d' ' -f1 | grep "^[^\#]" | xargs -i asdf plugin add {}
