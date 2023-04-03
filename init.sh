cc16d7e099761ec3c20441cf81497cd90d26a356
Xyven <git@xyven.dev>
2023-04-03 12:11:50 -0400

    eval hack for ubuntu

--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 echo '\n# asdf config' >> ~/.bashrc
 echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
-. ~/.bashrc
+eval "$(cat ~/.bashrc | tail -n +10)"
 asdf update
 
 cat ~/.tool-versions | cut -d' ' -f1 | grep "^[^\#]" | xargs -i asdf plugin add {}
