e5ff07c5d4fa1320e406a1802c760695e7404b98
Xyven <git@xyven.dev>
2023-04-03 12:29:41 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,24 @@
 FILE=~/.tool-versions
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install build-essential git -y
+
+if [[ -f $FILE ]]; then
+  git clone https://github.com/Xyven1/dotfiles.git ~/.temp_init
+  cd ~/.temp_init
+  cat .tool-versions >> ~/.tool-versions
+  cat .bashrc >> ~/.bashrc
+  cat .bash_aliases >> ~/.bash_aliases
+  sudo touch /.envrc
+  cat .envrc | sudo tee -a /.envrc
+  rm -rf ~/.asdf
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+  echo '\n# asdf config' >> ~/.bashrc
+  echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+  echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+fi
+echo "Please source .bashrc and then run this command again"
+  
 if command -v asdf &> /dev/null
 then
   asdf update
@@ -11,22 +31,5 @@ then
   asdf direnv setup --shell bash --version latest
   echo "Done setting things up. Please restart your machine to get everything working"
 else
-  sudo apt update && sudo apt upgrade -y
-  sudo apt install build-essential git -y
-  
-  if [[ -f $FILE ]]; then
-    git clone https://github.com/Xyven1/dotfiles.git ~/.temp_init
-    cd ~/.temp_init
-    cat .tool-versions >> ~/.tool-versions
-    cat .bashrc >> ~/.bashrc
-    cat .bash_aliases >> ~/.bash_aliases
-    sudo touch /.envrc
-    cat .envrc | sudo tee -a /.envrc
-    rm -rf ~/.asdf
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
-    echo '\n# asdf config' >> ~/.bashrc
-    echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-    echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
-  fi
-  echo "Please source .bashrc and then run this command again"
+  echo "'asdf' command not found, please make sure it is available"
 fi
